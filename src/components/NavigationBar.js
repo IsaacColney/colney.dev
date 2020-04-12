f95c8199ac9bb98ce5446ce2041747e34580626b
isaaccolney <isaaclalpekhluacolney@gmail.com>
2020-04-12 09:28:52 +0530

    add nav

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,7 +5,14 @@ const NavigationBar = () => {
         <div className='nav-bar'>
             <div className='content'>
                 <div className='btn'></div>
-                <div className='link'></div>
+                <div className='link'>
+                    <ul>
+                        <li>Home</li>
+                        <li>App</li>
+                        <li>Contact</li>
+                        <li>About</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
