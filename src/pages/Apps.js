ed2c431720c3a331f5a1605337440e84edbad0d5
isaaccolney <isaaclalpekhluacolney@gmail.com>
2020-04-18 11:20:44 +0530

    app card styling

--- src/pages/Apps.js
+++ src/pages/Apps.js
@@ -7,21 +7,10 @@ const Apps = () => {
     <div className="app-container">
       <h2>We got you with lots of usefull app</h2>
       <div className="row">
-        <AppCard
-          title="Klass"
-          style={{
-            backgroundColor: "rgb(100,40,40)",
-            "&:hover": { 
-               cursor: "pointer" },
-          }}
-        />
+        <AppCard title="Klass" className="klass" />
         <AppCard
           title="Covid-19"
-          style={{
-            backgroundColor: "rgb(40,40,70)",
-            "&:hover": {
-               cursor: "pointer" },
-          }}
+          className='covid-19'
         />
       </div>
     </div>
