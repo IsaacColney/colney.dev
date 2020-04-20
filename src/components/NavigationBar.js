6b5c3948e946552f6dbae288183f3a7888131f46
isaaccolney <isaaclalpekhluacolney@gmail.com>
2020-04-20 22:34:36 +0530

    fixed side drawer

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,73 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Navigation.scss";
 import Logo from "../assets/logo-sm.png";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "./Backdrop";
 
 const NavigationBar = (props) => {
-  const openDrawer = () => {
-    document.getElementsByClassName("drawer")[0].style.width = "50vw";
-    document.getElementsByClassName("drawerList")[0].style.display = "flex";
-  };
-  const closeDrawer = () => {
-    document.getElementsByClassName("drawer")[0].style.width = "0";
-    document.getElementsByClassName("drawerList")[0].style.display = "none";
-  };
+  const [showDrawer, setDrawer] = useState(false);
+   const openDrawer = () => {
+     setDrawer(true);
+     console.log(showDrawer);
+   }
 
+   const closeDrawer = () => {
+    setDrawer(false);
+    console.log(showDrawer);
+  }
   return (
-    <div className="nav-bar">
-      <div className="content">
-        <div className="nav-btn" onClick={openDrawer}>
-          <img src="./assets/icons/menu.png" alt="icons-menu" />
-        </div>
-        <div className="drawer">
-          <ul className="drawerList">
-            <img
-              src="./assets/icons/close.png"
-              onClick={closeDrawer}
-              alt="close"
-            />
-            <Link
-              to="/"
-              style={{ textDecoration: "none" }}
-              onClick={closeDrawer}
-            >
-              <li>Home</li>
-            </Link>
-            <Link
-              to="/apps"
-              style={{ textDecoration: "none" }}
-              onClick={closeDrawer}
-            >
-              <li>Apps</li>
-            </Link>
-            <Link
-              to="/about"
-              style={{ textDecoration: "none" }}
-              onClick={closeDrawer}
-            >
-              <li>About</li>
-            </Link>
-          </ul>
-        </div>
-        <Link to="/" className='logo'>
-          <img className="logo" src={Logo} alt="logo" />
-        </Link>
-        <div className="link">
-          <ul>
-            <Link to="/" style={{ textDecoration: "none" }}>
-              <li>Home</li>
-            </Link>
-            <Link to="/apps" style={{ textDecoration: "none" }}>
-              <li>Apps</li>
-            </Link>
-            <Link to="/about" style={{ textDecoration: "none" }}>
-              <li>About</li>
-            </Link>
-          </ul>
+    <React.Fragment>
+      {showDrawer ?? <Backdrop onClick={closeDrawer}/> }
+      <SideDrawer show={showDrawer} closeDrawer={closeDrawer} />
+      <div className="nav-bar">
+        <div className="content">
+          <div className="nav-btn" onClick={openDrawer}>
+            <img src="./assets/icons/menu.png" alt="icons-menu" />
+          </div>
+          <Link to="/" className="logo">
+            <img className="logo" src={Logo} alt="logo" />
+          </Link>
+          <div className="link">
+            <ul>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <li>Home</li>
+              </Link>
+              <Link to="/apps" style={{ textDecoration: "none" }}>
+                <li>Apps</li>
+              </Link>
+              <Link to="/about" style={{ textDecoration: "none" }}>
+                <li>About</li>
+              </Link>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
---
+++ src/components/SideDrawer.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+
+import "./SideDrawer.scss";
+
+const SideDrawer = (props) => {
+  const html = (
+    <div className="drawer">
+      <ul className="drawerList">
+        <img
+          src="./assets/icons/close.png"
+          onClick={props.closeDrawer}
+          alt="close"
+        />
+        <Link
+          to="/"
+          style={{ textDecoration: "none" }}
+          onClick={props.closeDrawer}
+        >
+          <li>Home</li>
+        </Link>
+        <Link
+          to="/apps"
+          style={{ textDecoration: "none" }}
+          onClick={props.closeDrawer}
+        >
+          <li>Apps</li>
+        </Link>
+        <Link
+          to="/about"
+          style={{ textDecoration: "none" }}
+          onClick={props.closeDrawer}
+        >
+          <li>About</li>
+        </Link>
+      </ul>
+    </div>
+  );
+
+  const content = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="sideDrawer-transition"
+      mountOnEnter
+      unmountOnExit
+    >
+      {html}
+    </CSSTransition>
+  );
+
+  return ReactDOM.createPortal(
+    content,
+    document.getElementById("sideDrawer-hook")
+  );
+};
+
+export default SideDrawer;
