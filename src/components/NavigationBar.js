5be1df919006aa42c2e1470a4ac60ae71c41f025
isaaccolney <isaaclalpekhluacolney@gmail.com>
2020-04-23 19:28:14 +0530

    counter

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,7 +7,7 @@ import SideDrawer from "./SideDrawer";
 import Backdrop from "./Backdrop";
 
 const NavigationBar = (props) => {
-  console.log('5');   
+  console.log('6');   
   const [showDrawer, setDrawer] = useState(false);
    const openDrawer = () => {
      setDrawer(true);
