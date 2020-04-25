6cfd9851730416454dbc85115ce2aaac8cf1e6aa
isaaccolney <isaaclalpekhluacolney@gmail.com>
2020-04-25 22:49:15 +0530

    change file

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./About.scss";
 
 const About = () => {
-  var counter = '5';
+  var counter = '1';
   return (
     <div className="about-container">
       <h1>About us</h1>
