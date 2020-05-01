2ecd590b115940b3e6003d50c092164342d4e1e7
isaaccolney <isaaclalpekhluacolney@gmail.com>
2020-05-01 16:17:56 +0530

    404 screen

--- src/components/AppCard.js
+++ src/components/AppCard.js
@@ -5,7 +5,7 @@ const AppCard = (props) => {
   return (
     <div className={`appcard-container ${props.className}`} style={props.style}>
       <h2>{props.title}</h2>
-      {props.appImg != null ? <img src={props.appImg} alt="app logo" /> : <h3>{props.appText}</h3>}
+      {props.appImg != null ? <img src={props.appImg} alt="app logo" /> : <h3>{props.description}</h3>}
       <p>{props.description}</p>
     </div>
   );
